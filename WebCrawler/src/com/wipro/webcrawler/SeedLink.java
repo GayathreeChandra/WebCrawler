5498cafc4346459346f42250ed946f4e4428d8e8
sthangamparthiban <sthangamparthiban@manh.com>
2018-11-18 08:43:42 +0530

    Webcrawler commit

---
+++ WebCrawler/src/com/wipro/webcrawler/SeedLink.java
@@ -0,0 +1,14 @@
+package com.wipro.webcrawler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SeedLink {
+	public static void main(String[] args){
+		List<Crawler> crawler=new ArrayList<Crawler>();
+		crawler.add(new Crawler("https://wiprodigital.com/"));
+		SearchLink searchlink=new SearchLink(crawler);
+		searchlink.search();
+	}
+
+}
